16ef551549801c121dfe0521e265f43acea1ba3e
pashupat <tushareshukla@gmail.com>
2024-04-02 07:45:52 +0530

    16

--- src/components/Sixteen.js
+++ src/components/Sixteen.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
 
 export default function Sixteen() {
+  const [color, setColor] = React.useState("#000000");
+  const handleColotChange = (e) => {
+    setColor(e.target.value);
+  };
   return (
-    <div>Sixteen</div>
-  )
+    <div>
+      <input type="color" onChange={handleColotChange} />
+      <br />
+      <br />
+
+      <br />
+
+      <br />
+
+      <br />
+
+    
+  <div
+          style={{display:"flex", gap:"10px"}}>
+  <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+ 
+    </div>
+  </div>
+  );
 }
